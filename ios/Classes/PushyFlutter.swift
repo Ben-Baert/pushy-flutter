5b57c471926977bcf5b3357ce63b695a184f23cb
Ben Baert <Ben-Baert@users.noreply.github.com>
2020-07-06 03:48:23 +0200

    Update PushyFlutter.swift

--- ios/Classes/PushyFlutter.swift
+++ ios/Classes/PushyFlutter.swift
@@ -3,10 +3,6 @@ import UIKit
 import Pushy
 
 public class PushyFlutter: NSObject, FlutterPlugin, FlutterStreamHandler {
-    var pushy: Pushy?
-    var eventSink: FlutterEventSink?
-    var hasStartupNotification = false
-    var startupNotification: [AnyHashable : Any]?
     
     public static func register(with registrar: FlutterPluginRegistrar) {
     }
