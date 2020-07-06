da74aca89542b03899c0f2b8c105576b7a96f3ae
Ben Baert <Ben-Baert@users.noreply.github.com>
2020-07-06 03:59:20 +0200

    Update PushyFlutter.swift

--- ios/Classes/PushyFlutter.swift
+++ ios/Classes/PushyFlutter.swift
@@ -1,6 +1,6 @@
 import Flutter
 
-public class PushyFlutter: NSObject, FlutterPlugin, FlutterStreamHandler {
+public class PushyFlutter: NSObject, FlutterPlugin {
     
     public static func register(with registrar: FlutterPluginRegistrar) {
     }
